7be8cefee656d0ee61b143cb05ec911ff0287a6e
unknown <tqtuan1509@gmail.com>
2022-11-28 14:25:18 +0700

    lan8

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -9,32 +9,35 @@ let getHomePage = (req, res) => {
 };
 
 let postWebhook = (req, res) => {
+  // Parse the request body from the POST
   let body = req.body;
 
-  console.log(`\u{1F7EA} Received webhook:`);
-  console.dir(body, { depth: null });
-  // Send a 200 OK response if this is a page webhook
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
 
-  if (body.object === "page") {
-    // Gets the body of the webhook event
-    let webhook_event = entry.messaging[0];
-    console.log(webhook_event);
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
+
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
 
 
-    // Get the sender PSID
-    let sender_psid = webhook_event.sender.id;
-    console.log('Sender PSID: ' + sender_psid);
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
 
-    // Check if the event is a message or postback and
-    // pass the event to the appropriate handler function
-    if (webhook_event.message) {
-      handleMessage(sender_psid, webhook_event.message);
-    } else if (webhook_event.postback) {
-      handlePostback(sender_psid, webhook_event.postback);
-    }
-    res.status(200).send("EVENT_RECEIVED");
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+    });
 
-    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
 
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
