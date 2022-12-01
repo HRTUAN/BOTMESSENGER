133cd12ab7f7ac36f59b1b8b2ce0f3e665388392
unknown <tqtuan1509@gmail.com>
2022-12-01 16:57:56 +0700

    lan16

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,15 +1,14 @@
 import express from "express";
-import homepageController from "../controllers/homepageController";
+import homeController from "../controllers/HomeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", homepageController.getHomePage);
-    router.get("/webhook", homepageController.getWebhook);
-    router.post("/webhook", homepageController.postWebhook);
-    router.post("/set-up-profile", homepageController.handleSetupProfile);
-    router.get("/set-up-profile", homepageController.getSetupProfilePage);
-    return app.use("/", router);
-};
+    router.get("/", homeController.getHomePage);
+
+    router.post('/webhook', homeController.postWebhook);
+    router.get('/webhook', homeController.getWebhook)
+    return app.use('/', router);
+}
 
 module.exports = initWebRoutes;
