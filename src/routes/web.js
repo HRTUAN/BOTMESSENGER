cadd014cf02a8f75d884560941c66a2d9cee7f99
unknown <tqtuan1509@gmail.com>
2022-12-01 19:01:06 +0700

    lan17

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,14 +1,21 @@
 import express from "express";
-import homeController from "../controllers/HomeController";
+import homepageController from "../controllers/homepageController";
+import chatBotController from "../controllers/chatBotController";
+import chatBotService from "../services/chatBotService";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", homeController.getHomePage);
+    router.get("/", homepageController.getHomepage);
+    router.get("/webhook", chatBotController.getWebhook);
+    router.post("/webhook", chatBotController.postWebhook);
+    router.get("/profile", homepageController.getFacebookUserProfile);
+    router.post("/set-up-user-fb-profile", homepageController.setUpUserFacebookProfile);
+    router.get("/test",async (req, res) =>{
+        let user = await chatBotService.getFacebookUsername(3350311028355090);
+    });
 
-    router.post('/webhook', homeController.postWebhook);
-    router.get('/webhook', homeController.getWebhook)
-    return app.use('/', router);
-}
+    return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
